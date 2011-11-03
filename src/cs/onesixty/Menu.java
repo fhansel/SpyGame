f1aa3a211e7adb9040155194025cd833a8b4e4c5
1337 |-|4x <kalask47@gmail.com>
2011-11-02 22:01:30 -0700

    menu stuff

--- src/cs/onesixty/Menu.java
+++ src/cs/onesixty/Menu.java
@@ -2,6 +2,7 @@ package cs.onesixty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Menu extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,10 @@ public class Menu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+       Button btnManageMissions = (Button)this.findViewById(R.id.button1);
+       Button btnManageFriends = (Button)this.findViewById(R.id.button2);
+       Button btnHighScores = (Button)this.findViewById(R.id.button3);
+       
+       
     }
 }
